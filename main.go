bb4a907dea6814a9580dd7c66143ccec83f7ca89
ruslangr <gizatulinrr@gmail.com>
2020-06-13 17:30:53 +0500

    Changes with interfaces§

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
-	var spBot spanishBot
-	var enBot englishBot
 
-	printGreetingSP(spBot)
-	printGreetingEB(enBot)
+	//var spBot spanishBot
+	//var enBot englishBot
+
+	enBot := englishBot{}
+	spBot := spanishBot{}
+
+	printGreeting(enBot)
+	printGreeting(spBot)
+
+	//printGreetingSP(spBot)
+	//printGreetingEB(enBot)
+}
+
+//Реализация printGreeting с интерфейсом:
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string { // (en englishBot) - en убрали так как en не используется в функции
@@ -21,6 +37,8 @@ func (spanishBot) getGreeting() string { // (sp englishBot) - sp убрали т
 	return ("Hola!")
 }
 
+/*---------------------------------------------
+Реализация без интерфецсов с 2мя разными функциями
 func printGreetingEB(eb englishBot) {
 	fmt.Println(eb.getGreeting()) // Сразу вызывается метод getGreeting для eb
 }
@@ -28,3 +46,4 @@ func printGreetingEB(eb englishBot) {
 func printGreetingSP(sb spanishBot) {
 	fmt.Println(sb.getGreeting()) // Сразу вызывается метод getGreeting для sb
 }
+-----------------------------------------------*/
